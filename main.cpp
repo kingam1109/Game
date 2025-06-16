46074796c15d90033a02a84e48bc5480fa209351
kingam1109 <kingamazurek18@wp.pl>
2025-06-16 22:13:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@
 #include <iostream>
 
 class Cat {
-public:
+public:  
     sf::Sprite sprite;
     sf::Clock timer;
     bool isVisible;
-
+//Constructor 
     Cat(sf::Texture& texture, sf::Vector2f position) {
         sprite.setTexture(texture);
         sprite.setPosition(position);
@@ -18,13 +18,13 @@ public:
         isVisible = true;
         timer.restart();
     }
-
+//erase cat
     void update() {
         if (timer.getElapsedTime().asSeconds() > 1.0f) {
             isVisible = false;
         }
     }
-
+//new cat
     void reset(sf::Texture& texture, sf::Vector2f position) {
         sprite.setTexture(texture);
         sprite.setPosition(position);
@@ -50,19 +50,20 @@ private:
     bool isGameRunning;
 
 public:
+//Constructor
     Game() : window(sf::VideoMode(800, 600), "Kotek Game"), score(0), timeLeft(30.0f), isGameRunning(true), currentCat(nullptr) {
         window.setFramerateLimit(60);
 
         if (!font.loadFromFile("arial.ttf")) {
             std::cerr << "Font loading failed!" << std::endl;
         }
-
+//set 3 cats textures
         for (int i = 0; i < 3; i++) {
             if (!catTexture[i].loadFromFile("kot" + std::to_string(i + 1) + ".png")) {
                 std::cerr << "Error loading texture!" << std::endl;
             }
         }
-
+// time and score text
         timeText.setFont(font);
         timeText.setCharacterSize(24);
         timeText.setFillColor(sf::Color::White);
@@ -72,7 +73,7 @@ public:
         scoreText.setCharacterSize(24);
         scoreText.setFillColor(sf::Color::White);
         scoreText.setPosition(10, 40);
-
+//background color
         bgColor = sf::Color(255, 105, 180);
     }
 
@@ -85,14 +86,14 @@ public:
             }
         }
     }
-
+//mouse clicks
     void handleEvents() {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-
+//mouse clik at cat
             if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
                 sf::Vector2i mousePos = sf::Mouse::getPosition(window);
                 if (currentCat != nullptr && currentCat->isVisible && currentCat->sprite.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
@@ -111,20 +112,20 @@ public:
             timeLeft = 0;
             isGameRunning = false;
         }
-
+//cat erase
         if (currentCat != nullptr) {
             currentCat->update();
         }
-
+//new cat
         if (currentCat == nullptr || !currentCat->isVisible) {
             spawnCat();
         }
-
+//color change
         int r = (bgColor.r + 1) % 256;
         int g = (bgColor.g + 1) % 256;
         int b = (bgColor.b + 1) % 256;
         bgColor = sf::Color(r, g, b);
-
+//time and score change
         timeText.setString("Time: " + std::to_string(static_cast<int>(timeLeft)));
         scoreText.setString("Score: " + std::to_string(score));
     }
@@ -143,7 +144,9 @@ public:
     }
 
     void spawnCat() {
+        //random position without leaving frame
         sf::Vector2f position(rand() % (window.getSize().x - 100), rand() % (window.getSize().y - 100));
+        //delete cat
         if (currentCat != nullptr) {
             delete currentCat;
         }
